832ba16094185ce1ab2a9e9f1d93450d2b406820
Miguel de Luis <luis.miguelde@gmail.com>
2020-05-05 23:28:00 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,8 @@
 
-const d6 = () => Math.floor(Math.random() * 6) + 1;
-// dice
-const twoD6 = () => d6() + d6();
-const threeD6 = () => d6() + d6() + d6(); 
-// rolls three dice
-
 function arrayToList(thisArray=[], first=false){
 	if(thisArray.length === 0 ) {
 		if(first){
-			return "---";
+			return "Empty.";
 		} else { 
 			return "."; 
 			   }
@@ -18,10 +12,34 @@ function arrayToList(thisArray=[], first=false){
 			// explicitly setting first to false because explicit is 
 			// better than implicit
 		} else {
-			return `, ${thisArray[0]}` + arrayToList(thisArray.slice(1), false);
+				return `, ${thisArray[0]}` + arrayToList(thisArray.slice(1), false);				
 		}
 	}
 }
+
+function max(thisArray, x=0){
+	if(thisArray.length === 0){
+		return x;
+	} 
+	x = thisArray[0] > x ? thisArray[0] : x ;
+	return max(thisArray.slice(1), x);
+}
+
+// ----- Dice ----------------------------------------------------------
+
+const d6 = () => Math.floor(Math.random() * 6) + 1;
+// dice
+const twoD6 = () => d6() + d6();
+const threeD6 = () => d6() + d6() + d6(); 
+// rolls three dice
+
+const weapon_list = new Map([
+	['hands', 0],
+	['dagger',1],
+	['spear',4],
+	['sword',3],
+	['',0],
+]);
 
 const armors = new Map([
   ['gambeson',3],
@@ -29,68 +47,168 @@ const armors = new Map([
   ['plate',7],
 ]);
 
-console.log(armors);
-console.log(armors.get('gambeson'));
+// ----- Rules ---------------------------------------------------------
+// 
+// Skill Roll
+// Skill + 3Dice >= Target Number 
+// Combat: 1 Roll Skill Roll + Modifiers > TN
+// normal TN = 19 goes from 12 to 30
 
-const section = {
-	title: "my title",
-	text: "<p>\
-	The text of each gamebook section contains one or more paragraph and\
-	other such block of texts. And even inline markup. Therefore it\
-	cannot be easily constrained to a format\
-	</p><p>\
-	For example here it's an <b>important</b> second paragraph</p>",
-	options: [1], //["This is option 1","This is option 2"], // an array it can be empty
-	};
-	
 const player = {
+	combatMod: function(weapon){
+		const x = weapon_list.get(weapon);
+		if(this.shield){return x+3} 
+		else if (weapon === "spear") {
+			return x - 2;
+		} else {return x}
+	},	
+	
+	maxCombatMod: function(){ 
+		return this.weapons.length === 0 ? this.combatMod("hands") 
+		                                 : max(this.weapons.map(w => this.combatMod(w)))
+		},
+		
+	skillRoll: function(difficulty=0) {	return this.skill >= threeD6() + difficulty ?  true : false	},
+	
+	combatRoll: function(enemyCombatMod) {return this.skillRoll( Math.floor((enemyCombatMod - this.maxCombatMod())/3)) },
+	
+	luck: function(difficulty=0){return threeD6() - 11 >= difficulty},
+	
 	name: "Pip", // to be entered by player Pip as default
-	skill: twoD6()+6,
-	stamina: threeD6(),
-	weapons: ["sword","dagger","bow"], // max 3 per game rules
-	shield: 0, // either shield or no shield, nothing fancy here
+	skill: 9,
+	weapons: ["sword", "pup"], // max 3 per game rules
+	shield: true, // either shield or no shield, nothing fancy here
 	armor: "gambeson", //
 	backpack: [], //max 12 per game rules
 	rations: 4,
 	money: 23, // in silver coins
 	show() {
-		const header = `<h3>Character Sheet</h3>`;
-		const main_stats = `<p><b>Name:</b> ${this.name} <b>Skill:</b> ${this.skill} <b>Stamina:</b> ${this.stamina}</p>`;
-		const weapon_list = `<p><b>Weapons:</b>  ${arrayToList(this.weapons, first=true)}</p>`;
-		return header + "\n" + main_stats + "\n" + weapon_list;
+		return `<h3>Character Sheet</h3>` +
+		       `<p><b>Name:</b> ${this.name} <b>Skill:</b> ${this.skill}` +
+		       `<p><b>Backpack:</b> ${arrayToList(this.backpack, first=true)}</p>` +
+			   `<p><b>Weapons:</b>  ${arrayToList(this.weapons, first=true)}</p>` +
+			   `<p><b>Rations:</b>  ${this.rations} <b>Money:</b>  ${this.money}</p>`;
 		},
 	};
+	
+// ----- Sections -------------------------------------------------------
 
-console.log( player.show());
-player.name = "Periwinkle";	
-title = "My title";
-const section_title = `<h2>${section.title}</h2>`;
-const section_text = section.text;
-console.log(section.options);
+sections = new Map();
 
-if(section.options.length === 0){options_text = "<h3>The End</h3>"} else {options_text = "<section><h3>Options</h3><ol><a class='options' href='dontcare'><li>First option</li></a></ol>"}
+sections.set("404", 
+		 {
+            title:"Error kinda 404:(",
+            text:"\
+            <p>Lo siento mucho, pero se ha generado un error en esta página. Lo\
+            más probable es que me haya olvidado de algo, o tenga un punto y\
+            coma donde no debería o que simplemente no hay todavía escrito la\
+            sección que habías elegido para continuar la aventura.</p>\
+            ",
+            choices: [],
+        });
+        
+sections.set("start", 
+		 {
+            title:"Game starts here",
+            text:"\
+            <p>This is where the game starts</p>\
+            ",
+            choices: [{text: "Get here", link:"here"},
+                      {text: "Get there", link:"there"} ],
+        });
+        
+sections.set("here", 
+		 {
+            title:"here",
+            text:"\
+            <p>This is where the game DOES NOT start</p>\
+            ",
+            choices: [{text: "Get here", link:"here"},{text: "Get there", link:"there"} ],
+            hasTest: false,
+            hasTest: false,
+        });
+        
+sections.set("there", 
+		 {
+            title:"there",
+            text:"\
+            <p>There is where the game ends</p>\
+            ",
+            choices: [],
+            hasTest: true,
+            diff: 0,
+            altTitle: "we game on!",
+            altText: "<p>yay, gaming is fun!</p>",
+            altChoices: [{text: "Get here", link:"here"},{text: "Get there", link:"there"} ],
+        });
+        
+showChoice = choice => `<li><a class='choice' href='${choice.link}'>${choice.text}</a></li>`
+        
+showChoices = choices => choices.length === 0 ? "" : showChoice(choices[0]) + showChoices(choices.slice(1)) ;
 
-document.getElementById("gbSection").innerHTML = section_title + 
-												 section.text + options_text;
-												 
-document.getElementById("characterSheet").innerHTML = player.show();												 
 
-const options = document.getElementsByClassName("options");
-// Creates a special object, which is kinda an array but not quite
-// So I can't use forEach etc unless using Array.from(options);
-// discovered it using  console.log(typeof options);
 
-// old school way to do it
-//for (i = 0; i < options.length; i++) {
-//  options.item(i).addEventListener("click", function(click){
-//  click.preventDefault();
-//  alert("Managed by the gamebook engine");
-//});
-//}
+function showSection(section, error = sections.get("404")){
+	section = typeof section === "undefined" ? error : section;
+	let title = section.title;
+	let text = section.text;
+	let choices = section.choices;
+	
+	if(section.hasTest && player.skillRoll(player.diff)){
+		title = section.altTitle;	
+		text = section.altText;
+		choices = section.altChoices;	
+	} 
+	x = `<h2>${title}</h2>`
+	x += text;	
+	x += section.length === 0 ? "<h3>The End</h3>" : `<ol>${showChoices(choices)}</ol>`;
+	return x ;
+	
+}
 
-Array.from(options).forEach( link =>
+function updateDocument(sectionKey="start"){
+	const thisSection = sections.get(sectionKey)
+	document.getElementById("gbSection").innerHTML = showSection(thisSection);
+	document.getElementById("characterSheet").innerHTML = player.show();		
+	const choices = document.getElementsByClassName("choice");
+	Array.from(choices).forEach( link =>
 		link.addEventListener("click", function(click){
 		click.preventDefault();
-		alert("Managed by the gamebook engine");
+		updateDocument(link.getAttribute("href"));
 	})
 );
+										 
+}
+
+updateDocument();
+
+
+
+
+// ----- Stats ---------------------------------------------------------
+
+//for(diff = -6; diff <= 6; diff += 1){
+	//var successes = 0;
+	//for(x=0; x < 1000; x +=1){	
+		//successes += player.luck(diff) ? 1 : 0;
+		//}
+	//console.log(`Luck roll at diff ${diff}: ${successes}`);
+//}
+	
+//for(diff = -6; diff <= 6; diff += 1){
+	//var successes = 0;
+	//for(x=0; x < 1000; x +=1){	
+		//successes += player.skillRoll(diff) ? 1 : 0;
+		//}
+	//console.log(`Skill roll at diff ${diff}: ${successes}`);
+//}
+
+
+//for(eCM = -3; eCM < 35; eCM += 1){
+	//var victories = 0;
+	//for(x=0; x < 1000; x +=1){	
+		//victories += player.combatRoll(eCM) ? 1 : 0;
+		//}
+	//console.log(`Player Combat maxMod ${player.maxCombatMod()}`);
+	//console.log(`Combat at enemyCombatMod ${eCM}: ${victories}`);
+//}
